ec268ba51b8078230c30a51717334cb485139ad5
DBuret <david.buret@gmail.com>
2018-12-30 15:30:56 +0100

    build

--- buildit.sh
+++ buildit.sh
@@ -1,7 +1,5 @@
 GGO_ENABLED=0 GOOS=linux go build -a -tags netgo -ldflags "-w" .
 strip gse
+chmod 755 gse
 sudo docker build -t gse .
-sudo docker save gse > gse.tar
-gzip gse.tar
-ls -l gse.tar.gz
 sudo docker images
