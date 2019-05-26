82fe6e6ccbfcbc410ba3aed0b946ae47013626b8
David Buret <rmotfage@gmail.com>
2019-05-26 16:34:31 +0200

    chmod build

--- buildit.sh
+++ buildit.sh
@@ -1,5 +1,7 @@
 GGO_ENABLED=0 GOOS=linux go build -a -tags netgo -ldflags "-s -w" .
 upx --brute gse
+chmod 555 gse
+chmod 444 template.html
 sudo docker build -t gse .
 sudo docker save gse > gse.tar
 gzip gse.tar
